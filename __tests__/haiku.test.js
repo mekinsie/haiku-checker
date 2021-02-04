22130bad3f04c5d0669c75b525fd875f1be4d40f
Jo <joannadawnmiller@gmail.com>
2021-02-04 14:51:16 -0500

    fail first line2Count test
    Co-authored-by: Levi Kohler <kohler.la01@gmail.com>

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -13,27 +13,9 @@ test("it should count the syllabels in line 1", () => {
     const haiku = new Haiku("Hello please, count my stale beautiful syllable for me","","");
     expect(haiku.line1Count()).toEqual(14);
   });
+
+  test("it should count the syllabels in line 2", () => {
+    const haiku = new Haiku("","Hello please, count my stale beautiful syllable for me","");
+    expect(haiku.line2Count()).toEqual(14);
+  });
 })
-
-
-
-
-
-
-
-
-
-
-
-
-// test("it should return an array of vowels from a string", () => {
-//   const haiku = new Haiku("","Hello","");
-//   expect(haiku.line2Count).toEqual(["e","o"]);
-// });
-
-// test("it should return an array of vowels from a string", () => {
-//   const haiku = new Haiku("","","Hello");
-//   expect(haiku.line3Count()).toEqual(["e","o"]);
-// });
-// test("it should return the number of elements in an array", () => {
-  //   expect(haiku.vowel)
--- src/haiku.js
+++ src/haiku.js
@@ -1,17 +1,3 @@
-// Identify vowels
-
-// Identify consonants
-
-// Identify number of lines in poem 
-
-// Identify first line has 5 syllables
-
-// Identify second line has 7 syllables
-
-// Identify third line has 5 syllables
-
-// If 2 consonants are together they get split, except when they are put together for a single sound (ie: th, ph, ng)
-
 export default class Haiku {
   constructor(line1, line2, line3) {
     this.line1 = line1;
@@ -24,7 +10,7 @@ export default class Haiku {
     firstString = firstString.replace(/\W/g," ");
     let line1Array = firstString.match(/[aeiouy]/gi);
     let line1Syl = line1Array.length;
-    
+
     line1Array = firstString.match(/\w{2}e\s+/gi);
     line1Syl -= line1Array.length;
 
@@ -39,26 +25,8 @@ export default class Haiku {
 
     return line1Syl;
   }
+
+  line2Count() {
+
+  }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-// line2Count() {
-//   let line2Array = this.line2.match(/[aeiouy]/gi);
-//   return line2Array;
-// }
-
-// line3Count() {
-//   let line3Array = this.line3.match(/[aeiouy]/gi);
-//   return line3Array;
-// }
