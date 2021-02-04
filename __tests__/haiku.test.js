e8b77e65d2b4cdf0ab8536b531dff01fb72012e4
Jo <joannadawnmiller@gmail.com>
2021-02-04 18:06:44 -0500

    refactor code and tests
    Co-authored-by: Levi Kohler <kohler.la01@gmail.com>

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -9,26 +9,6 @@ test("should correctly create a haiku object with three keys", () => {
     expect(haiku.line3).toEqual("");
   });
 
-// test("it should count the syllabels in line 1", () => {
-//     const haiku = new Haiku("Hello please, count my stale beautiful syllable for me","","");
-//     expect(haiku.line1Count()).toEqual(14);
-//   });
-
-//   test("it should count the syllabels in line 2", () => {
-//     const haiku = new Haiku("","Hello please, count my stale beautiful syllable for me","");
-//     expect(haiku.line2Count()).toEqual(14);
-//   });
-
-  test("it should count the syllabels in line 3", () => {
-    const haiku = new Haiku("","","hi can you read me");
-    expect(haiku.line3Count()).toEqual(5);
-  });
-
-  test("it should count the syllabels in line 3", () => {
-    const haiku = new Haiku("","","Hello please, count my stale beautiful syllable for me");
-    expect(haiku.line3Count()).toEqual(14);
-  });
-
   test("it should count the syllables in line1 of the Haiku object", () => {
     const haiku = new Haiku("Hello please, count my stale beautiful syllable for me","","");
     let arrayOfStrings = (Object.values(haiku));
--- src/haiku.js
+++ src/haiku.js
@@ -5,72 +5,6 @@ export default class Haiku {
     this.line3 = line3;
   }
 
-  // line1Count() {
-  //   let firstString = `${this.line1} `;
-  //   firstString = firstString.replace(/\W/g," ");
-  //   let line1Array = firstString.match(/[aeiouy]/gi);
-  //   let line1Syl = line1Array.length;
-
-  //   line1Array = firstString.match(/\w{2}e\s+/gi);
-  //   line1Syl -= line1Array.length;
-
-  //   line1Array = firstString.match(/[aeiouy]{2}/gi);
-  //   line1Syl -= line1Array.length;
-
-  //   line1Array = firstString.match(/[aeiouy]{3}/gi);
-  //   line1Syl -= line1Array.length;
-
-  //   line1Array = firstString.match(/[^aeiouy]+le\s+/gi);
-  //   line1Syl += line1Array.length;
-
-  //   return line1Syl;
-  // }
-
-  // line2Count() {
-  //   let secondString = `${this.line2} `;
-  //   secondString = secondString.replace(/\W/g," ");
-  //   let line2Array = secondString.match(/[aeiouy]/gi);
-  //   let line2Syl = line2Array.length;
-
-  //   line2Array = secondString.match(/\w{2}e\s+/gi);
-  //   line2Syl -= line2Array.length;
-
-  //   line2Array = secondString.match(/[aeiouy]{2}/gi);
-  //   line2Syl -= line2Array.length;
-
-  //   line2Array = secondString.match(/[aeiouy]{3}/gi);
-  //   line2Syl -= line2Array.length;
-
-  //   line2Array = secondString.match(/[^aeiouy]+le\s+/gi);
-  //   line2Syl += line2Array.length;
-
-  //   return line2Syl;
-  // }
-
-  line3Count() {
-    let thirdString = `${this.line3} `;
-    thirdString = thirdString.replace(/\W/g," ");
-    let line3Array = thirdString.match(/[aeiouy]/gi);
-    let line3Syl = line3Array.length;
-
-    if (thirdString.match(/\w{2}e\s+/gi) != null) {
-      line3Array = thirdString.match(/\w{2}e\s+/gi)
-      line3Syl -= line3Array.length;
-    }
-    if (thirdString.match(/[aeiouy]{2}/gi) != null) {
-      line3Array = thirdString.match(/[aeiouy]{2}/gi);
-      line3Syl -= line3Array.length;
-    }  
-    if (thirdString.match(/[aeiouy]{3}/gi) != null) {
-      line3Array = thirdString.match(/[aeiouy]{3}/gi);
-      line3Syl -= line3Array.length;
-    }  
-    if (thirdString.match(/[^aeiouy]+le\s+/gi) != null) {
-      line3Array = thirdString.match(/[^aeiouy]+le\s+/gi);
-      line3Syl += line3Array.length;
-    }
-    return line3Syl;
-  }
 
   syllableCounter(str) {
     let lineString = `${str} `;
@@ -95,7 +29,6 @@ export default class Haiku {
       lineSyl += lineArray.length;
     }
     return lineSyl;
-
   }
 
 
