86eeaf87f2ae61dafeda8e8e1482da6e56462015
Jo <joannadawnmiller@gmail.com>
2021-02-04 19:55:26 -0500

    add UI logic
    Co-authored-by: Levi Kohler <kohler.la01@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,4 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Haiku from "./haiku.js";
 
+$(document).ready(function() {
+  $("#haikuForm").submit(function(event) {
+    event.preventDefault();
+    const input1 = $("#line1Input").val();
+    const input2 = $("#line2Input").val();
+    const input3 = $("#line3Input").val();
+    const haiku = new Haiku(input1, input2, input3);
+    const response = haiku.haikuChecker();
+    if (response === true) {
+      $("#isHaiku").show();
+      $("#notHaiku").hide();
+    } else { 
+      $("#notHaiku").show();
+      $("#isHaiku").hide();
+    }
+  });
+});
